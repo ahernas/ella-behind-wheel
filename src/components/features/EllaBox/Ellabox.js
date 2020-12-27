7406e962723ea4390d179ad693fef82730a146cf
Anna Garczarek <ania.garczarek@gmail.com>
2020-12-27 03:58:44 +0100

    Add section contact

--- src/components/features/EllaBox/Ellabox.js
+++ src/components/features/EllaBox/Ellabox.js
@@ -13,19 +13,21 @@ const EllaBox = () => (
           Zapraszamy Panią Elżbietę Garczarek na jazdy doszkalające!
       </div>
     </div>
-    <div className={'d-flex align-items-center justify-content-center ' + styles.box}>
+    <div className={'d-flex align-items-center justify-content-center mb-5 ' + styles.box}>
       <div className='container'>
             W ofercie 4 godziny prowadzenia auta z instruktorem. <br /> Prosimy o wybranie odpowiedniego możliwego terminu i uzgodnienie tego <br/> z jednym  z naszych konsultantów.
       </div>
     </div>
-    <div className={'mt-3 mb-3 d-flex align-items-center justify-content-between container ' + styles.contact}>
-      <div className='d-flex col '>
+    <div className={'mt-3 mb-3 d-flex align-items-center justify-content-around container ' + styles.contact}>
+      <div className={'d-flex align-items-center ' + styles.contactBox}>
         <img src='https://i.postimg.cc/8Csb3XqR/anna.jpg'/>
-        <span>Anna Hernas</span>796 666 870
+        <div className={'d-flex align-items-center justify-content-center mr-0 mt-2 ' + styles.name}>Anna Hernas</div>
+        <div className={'d-flex align-items-center justify-content-center'}>796 666 870</div>
       </div>
-      <div className='d-flex col '>
+      <div className={'d-flex align-items-center '+ styles.contactBox}>
         <img src='https://i.postimg.cc/c4ZBZLp4/micha.jpg'/>
-        <span>Michał Hernas</span> 577 652 787
+        <div className={'d-flex align-items-center justify-content-center mr-0 mt-2 ' + styles.name}>Michał Hernas</div>
+        <div className={'d-flex align-items-center justify-content-center'}>577 652 787</div>
       </div>
     </div>
   </div>
